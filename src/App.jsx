6c27fc99586ef0213e2d15343ec0a5399c65dacc
Dipanshu Kapoor <dipanshu.kapoor2002@gmail.com>
2023-12-13 21:53:24 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux' 
 import authService from './appwrite/auth'
-import {login,logout} from './store/authSlice'
+import { login, logout } from './store/authSlice'
 import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
 
@@ -10,26 +10,29 @@ import './App.css'
 function App() {
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
+
   useEffect(() => {
-      authService.getCurrentUser()
-        .then((userData) => {
-          if (userData) {
-            dispatch(login({userData}))
-          } else{
-            dispatch(logout())
-          }
-        })
-        .finally(() => setLoading(false))
-  })
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [dispatch])
 
   return !loading ? (
-    <div className='min-h-screen flex-wrap flex content-between bg-black '>
+    <div className='min-h-screen flex-wrap flex content-between bg-black'>
       <div className='w-full min-h-screen flex-wrap content-between bg-black'>
-        < Header />
-        <main>
-          <Outlet />
-        </main>
-        < Footer />
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main style={{ flex: 1 }}>
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
       </div>
     </div>
   ) : null
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <div>
-      <img src="https://img.buzzfeed.com/buzzfeed-static/complex/images/ic7defqune831dh9orwm/memoji-2.png" alt="" style={{width:width}} />
+      <img src="https://img.buzzfeed.com/buzzfeed-static/complex/images/ic7defqune831dh9orwm/memoji-2.png" alt="" style={{width:width, }} />
     </div>
   )
 }
