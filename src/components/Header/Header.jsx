ce57512db01c85a075a3bc9c93c6893f111f0280
Dipanshu Kapoor <dipanshu.kapoor2002@gmail.com>
2023-12-15 01:21:55 +0530

    login padding fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useEffect, useState} from "react";
 import { Container, Logo, LogoutBtn } from "../index";
 import { useNavigate, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -6,6 +6,7 @@ import { useSelector } from "react-redux";
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const navItems = [
     {
       name: "Home",
@@ -34,30 +35,46 @@ function Header() {
     },
   ];
 
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const buttonFontSize = Math.max(12, windowWidth / 30); // Adjust the division value as needed
+
   return (
     <header className="py-3 bg-gradient-to-r from-violet-500 to-fuchsia-500 w-full rounded-lg">
       <Container>
-        <nav className="flex">
+        <nav className="flex flex-wrap justify-between items-center">
           <div className="mr-5">
             <Link to="/">
               <Logo width="70px" />
             </Link>
           </div>
-          <ul className="flex ml-auto">
-            {navItems.map((item) =>
-              item.active ? (
-                <li key={item.name}>
-                  <button
-                    onClick={() => navigate(item.slug)}
-                    className="inline-block px-6 py-4 bg-gradient-to-r from-sky-500 to-indigo-500 rounded-full text-white shadow-lg shadow-indigo-500/50"
-                  >
-                    <b>{item.name}</b>
-                  </button>
-                </li>
-              ) : null
+          <ul className="flex flex-wrap items-center">
+            {navItems.map(
+              (item) =>
+                item.active && (
+                  <li key={item.name} className="mb-3">
+                    <button
+                      onClick={() => navigate(item.slug)}
+                      className="px-3 py-1 bg-gradient-to-r from-sky-500 to-indigo-500 rounded-full text-white shadow-lg shadow-indigo-500/50"
+                      style={{ fontSize: `${buttonFontSize}px` }}
+                    >
+                      <b>{item.name}</b>
+                    </button>
+                  </li>
+                )
             )}
             {authStatus && (
-              <li>
+              <li className="mb-3">
                 <LogoutBtn />
               </li>
             )}
